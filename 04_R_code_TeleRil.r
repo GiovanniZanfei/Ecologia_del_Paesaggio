d5e806e39890b8070ea68eb9fdd07e75b35c3c91
GiovanniZanfei <62332774+GiovanniZanfei@users.noreply.github.com>
2020-07-08 16:26:55 +0200

    Update 04_R_code_TeleRil.r

--- 04_R_code_TeleRil.r
+++ 04_R_code_TeleRil.r
@@ -1,35 +1,37 @@
-# Codice r per analisi satellitari
+# Codice R per analisi satellitari (telerilevamento)
 
-# Packages
-install.packages("raster")
-library(raster)
+#  set wd e pacchetti ("raster","RStoolbox")  
 setwd("C:/lab")
-install.packages("RStoolbox")
+install.packages("raster")       # "raster" per lettura, scrittura, analisi e modellizzazione di dati spaziali
+library(raster)
+install.packages("RStoolbox")    # "RStoolbox" per analisi dati mediante telerilevamento
 
-# Funzione brick per importare un immagine selezionata
+# funzione "brick" per importare immagine selezionata e creare ogetto "RasterBrick" (multistrato)
 p224r63_2011<-brick("p224r63_2011_masked.grd")
-plot(p224r63_2011) # Ci dà 7 riquadri che mostrano un'immagine basata sulla riflettanza a varie lunghezze d'onda, come indicato sotto
+
+# plot oggetto appena creato
+plot(p224r63_2011) # 7 riquadri che mostrano un'immagine basata su riflettanza a varie lunghezze d'onda, come indicato sotto
 # B1: blue, B2: green, B3: red, B4: near infrared (nir), B5: medium infrared, B6: thermal infrared, B7: medium infrared
 
-# Creo una RampPalette che assegno all'oggetto cl per avere le immagini con scala di colori da bianco a nero una volta rifatto il comando plot con specifica del colore
+# RampPalette ("cl") per avere immagini con scala di colori da bianco a nero una volta rifatto il comando plot con specifica del colore
 cl<-colorRampPalette(c('black','grey','light grey'))(100) 
 plot(p224r63_2011,col=cl)
 
-# Scala del grigio con meno colori
+# modifica scala cromatica (da 100 a 5 sfumature)
 cllow<-colorRampPalette(c('black','grey','light grey'))(5)
 plot(p224r63_2011,col=cllow)
 
-# Plot della banda blu (B1)
-names(p224r63_2011)
+# plot banda blu (B1)
+names(p224r63_2011)                                             # "names" -> visionare nomi oggetto
 # [1] "B1_sre" "B2_sre" "B3_sre" "B4_sre" "B5_sre" "B6_bt"  "B7_sre"
-clb<-colorRampPalette(c('dark blue','blue','light blue'))(100)
+clb<-colorRampPalette(c('dark blue','blue','light blue'))(100)  # palette blu
 plot(p224r63_2011$B1_sre,col=clb)
 
-# Esercizio: plottare la banda dell'infrarosso vicino con RampPalette che varia dal rosso, all'arancione, al giallo
+# esercizio: plottare banda infrarosso vicino palette rosso-arancione-giallo
 clnir<-colorRampPalette(c('red','orange','yellow'))(100)
 plot(p224r63_2011$B4_sre,col=clnir)
 
-# 4 bande, plot multiframe
+# plot multiframe, quattro bande
 par(mfrow=c(2,2))
 # blue
 clb<-colorRampPalette(c('dark blue','blue','light blue'))(100)  
@@ -44,77 +46,98 @@ plot(p224r63_2011$B3_sre,col=clr)
 clnir<-colorRampPalette(c('red','orange','yellow'))(100)
 plot(p224r63_2011$B4_sre,col=clnir)
 
-# dev.off per chiudere
+# "dev.off()" per chiudere
 dev.off()
 
 # natural colours
 # 3 componenti: R G B
-# 3 bande: R = banda del rosso, G = banda del verde, B = banda del blu
+# 3 bande: R = banda rosso, G = banda verde, B = banda blu
 # B1: blue - 1
 # B2: green - 2
 # B3: red - 3
 # B4: near infrared (nir) - 4 
 
-# plotrgb: no!
-plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin") # stretch="Lin" per allargare l'immagine in modo lineare
+# "plotRGB" -> creare plot rosso-verde-blu su tre livelli (tre strati combinati per rappresentare bande rosso, verde e blu)
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin") # stretch="Lin" per migliorare visibilità immagine
 
-# nir
-# false colours
+# nir => aggiunta banda infrarosso per rendere immagine più leggibile (necessario togliere una delle altre tre, in questo caso blu)
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+# permette di visualizzare vegetazione
 
-# salvataggio
+# salvataggio immagine appena ottenuta
 pdf("primografico.pdf") 
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 dev.off()
 
-# multiframe
+# multiframe bande diverse
 par(mfrow=c(2,1))
 plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 dev.off()
 
-# nir nella compnente R(Red)
+# esercizio: nir nella compnente R(Red)
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
-# ESercizio: nir nella componente G(Green)
+
+# esercizio: nir nella componente G(Green)
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
-# Esercizo: nir nella componente B(Blue)
+
+# esercizo: nir nella componente B(Blue)
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
 
+# landcover
+
+# setwd e pacchetti
+setwd("C:/lab/p224r63")
+library(raster)
+
+# "brick" per importare immagine
+p224r63_2011<-brick("p224r63_2011_masked.grd")
+
+# richiamare "RStoolbox"
+library(RStoolbox)
+
+# plottare immagine in R G B
+plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
+
+# classificazione dati raster con "unsuperClass", specificando numero di classi
+p224r63_2011c<-unsuperClass(p224r63_2011,nClasses = 4)
+
+# visualizzare nuovo modello contenente anche mappa
+p224r63_2011c
+
+# plot mappa (quattro colori -> quattro classi specificate) 
+plot(p224r63_2011c$map)
+
+# nuova palette (migliore visualizzazione del grafico)
+clclass <- colorRampPalette(c('green',"red","blue","black"))(100)
+plot(p224r63_2011c$map,col=clclass)
+
 # Day2
+# setwd e pacchetti
 library(raster)
 setwd("C:/lab")
 load("TeleRil.RData")
 ls()
 
-# Funzione brick per importare un immagine selezionata, poi plot
-p224r63_1988<-brick("p224r63_1988_masked.grd")
-plot(p224r63_1988)
+# importare file 1988 e 2011 ("brick")
+p224r63_2011 <- brick("p224r63_2011_masked.grd") 
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
 
-# plot multiframe
-par(mfrow=c(2,2))
-# blue
-clb<-colorRampPalette(c('dark blue','blue','light blue'))(100)  
-plot(p224r63_1988$B1_sre,col=clb)
-# green
-clg<-colorRampPalette(c('dark green','green','light green'))(100) 
-plot(p224r63_1988$B2_sre,col=clg)
-# red
-clr<-colorRampPalette(c('dark red','red','pink'))(100)  
-plot(p224r63_1988$B3_sre,col=clr)
-# nir
-clnir<-colorRampPalette(c('red','orange','yellow'))(100)
-plot(p224r63_1988$B4_sre,col=clnir)
-
-dev.off()
-
-# Nuovo plotRGB
+# immagine 1988, come 2011 ha bande (colori):
 # B1: blue - 1
 # B2: green - 2
 # B3: red - 3
 # B4: near infrared (nir) - 4 
-plotRGB(p224r63_1988,r=3,g=2,b=1,stretch="Lin")
+# B4: near infrared (nir)
+# B5: medium infrared
+# B6: thermal infrared
+# B7: medium infrared
 
-# Esercizio: plot usando nir nella componenete R di RGB
+# plot oggetto 1988 e visualizzare campi
+plot(p224r63_1988)
+names(p224r63_1988)
+
+# esercizio: plot usando nir nella componenete R di RGB
 plotRGB(p224r63_1988,r=4,g=2,b=1,stretch="Lin")
 
 # plot immagini 1988 e 2011
