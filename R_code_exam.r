3843b56693902e1890880597d940072f159e843d
GiovanniZanfei <62332774+GiovanniZanfei@users.noreply.github.com>
2020-07-08 15:35:49 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -5,19 +5,18 @@
 
 # 1. 01_R_code_first.r	        
 # 2. 02_R_code_spatial.r	
-# 3. R_code_spatial2.r    #accorpato a spatial
-# 4. 03_R_code_point_patterns.r
-# 5. 04_R_code_TeleRil.r	
-# 6. 05_R_code_Landcover.r	
-# 7. R_code_multitemp.r	
-# 8. R_code_multitemp_NO2.r	
-# 9. R_code_snow.r	            #da far
-# 10. R_code_patches.r          # da far
+# 3. 03_R_code_point_patterns.r
+# 4. 04_R_code_TeleRil.r	
+# 5. 05_R_code_Landcover.r	
+# 6. R_code_multitemp.r	
+# 7. R_code_multitemp_NO2.r	
+# 8. R_code_snow.r	            #da far
+# 9. R_code_patches.r          # da far
 
 #############################################################################
 #############################################################################
 
-# 1. R_code_first.r - Primo codice R Ecologia del Paesaggio
+# 1. 01_R_code_first.r - Primo codice R Ecologia del Paesaggio
 
 # GZ libraries: "install.packages()" per scaricare libraries che posso poi richiamare con comando "library()" [o "require()]
 install.packages("sp")  
@@ -93,7 +92,7 @@ pairs(meuse[,3:6],lower.panel=panel.smoothing,upper.panel=panel.correlations,dia
 #######################################################
 #######################################################
 
-# 2. R spaziale: funzioni sapziali in Ecologia del Paesaggio [24/03/2020]
+# 2. 02_R_code_spatial.r - Funzioni sapziali in Ecologia del Paesaggio [24/03/2020]
 
 # GZ caricare library e dati
 library(sp)
@@ -136,7 +135,7 @@ plot(meuse)
 # GZ "spplot()" -> distribuzione spaziale di una variabile (in questo caso "zinc")
 spplot(meuse,"zinc")
 
-### Spatial-2 [25/03/2020]
+# Spatial-2 [25/03/2020]
 
 # GZ installare library "sp", caricare dati "meuse" e fissare dataset ["attach()"]
 install.packages("sp")
@@ -177,6 +176,187 @@ Covid19<-read.table("covid_agg.csv",head=T)  # GZ intitolare tabella "Covid19"
 
 #######################################################
 #######################################################
+
+# 03_R_code_point_patterns.r - Analisi point patterns [25/03/2020]
+
+# GZ installare libraries ("ggplot2", "spatstat")
+intall.packages("ggplot2")
+library(ggplot2)
+install.packages("spatstat)
+library(spatstat)
+
+# GZ set working directory
+setwd("C:/lab")
+
+# GZ importare tabella dati Covid19; "head=T" per indicare a R che ci sono i titoli delle colonne; dare alla tabella il nome Covid19
+Covid19<-read.table("covid_agg.csv",head=T)
+head(Covid19)   # comando per vedere tabella
+
+# GZ creare plot che associa Paesi e casi di Covid19 (anzichè "$" si può fare "attach(Covid19) plot(country,cases)")
+plot(Covid19$country,Covid19$cases)
+
+# GZ modificare struttura plot -> posizione etichette rispetto ad asse ("las=0" etichette parallele, 1 orizzontali, 2 perpendicolari, 3 verticali)
+plot(Covid19$country,Covid19$cases,las=0)
+plot(Covid19$country,Covid19$cases,las=1)
+plot(Covid19$country,Covid19$cases,las=2)
+plot(Covid19$country,Covid19$cases,las=3)
+plot(Covid19$country,Covid19$cases,las=3,cex.axis=0.5) # GZ "cex.axis" -> rimpicciolire dimensione etichette
+
+# GZ richiamare "ggplot2" (pacchetto per estetica e cura dei dettagli)
+library(ggplot2)
+
+# GZ dataframe "mpg" da library "ggplot2" ("mpg" raccoglie osservazioni US Environmental Protection Agency su 38 modelli di auto)
+data(mpg)
+head(mpg)
+
+# GZ esempio di plot con 2 variabili numeriche -> ggplot2 ha bisogno di 3 cose: dati ("mpg"), estetica del grafico ("aes", funzione di quotazione) e geometria ("geom_")
+ggplot(mpg,aes(x=displ,y=hwy))+geom_point()
+ggplot(mpg,aes(x=displ,y=hwy))+geom_line()    # GZ usare linee anzichè punti nella visualizzazione
+ggplot(mpg,aes(x=displ,y=hwy))+geom_polygon() # GZ usare poligoni 
+
+# GZ "ggplot2" per dati Covid19 -> usare longitudine e latitudine per avere i punti nello spazio, "size=cases" -> punti più grandi dove ci sono più casi
+ggplot(Covid19,aes(x=lon,y=lat,size=cases))+geom_point() 
+
+# GZ richiamare library "spatstat" (mostra analisi dei modelli dei punti spaziali) e fissare dataframe
+library(spatstat)
+attach(covid)
+
+# GZ esercizio: zona con più alta densità casi di Covid19
+# GZ creare dataset per spatstat -> "ppp" crea un oggetto che rappresenta un insieme di dati del pattern puntiforme nel piano bidimensionale
+covids<-ppp(lon,lat,c(-180,180),c(-90,90))  # GZ necessario specificare cosa indicano x e y ("lon","lat") e definirne il range
+d<-density(covids)                          # GZ comando per calcolare densità dei casi
+plot(d)                                     # GZ plot (rappresentazione grafica) densità
+points(covids,pch=19)                       # GZ mostare i punti Covid19 sulla mappa di densità
+
+# point patterns-2 [01/04/20]
+
+# GZ settare wd, caricare file salvato, richiamare "spatstat" e mostrare grafico densità casi Covid
+setwd("C:/lab")
+load("point_pattern.RData")
+ls()   # per vedere contenuto del file caricato
+library(spatstat)
+plot(d)
+
+# GZ palette -> modificare colori del plot d; (100) per dire a R quante sfumature deve avere la scala di colori
+cl<-colorRampPalette(c('yellow','orange','red')) (100)
+plot(d,col=cl)                                               # GZ plot densità con nuovi colori
+# esercizio: plot densità dal verde al blu
+bluverde<-colorRampPalette(c('blue','grey','green')) (200)
+plot(d,col=bluverde)
+
+# GZ mostare punti Covid19 sulla mappa di densità
+points(covids)
+
+# GZ inserire nella mappa confini degli stati
+install.packages("rgdal")                   # GZ "rgdal" -> library necessaria per usare il comando "readOGR"
+library(rgdal)
+coastlines<-readOGR("ne_10m_coastline.shp") # GZ "readOGR" -> funzione che legge origine dati OGR e un layer in un oggetto vettoriale spaziale adatto, serve per creare layer dei confini
+plot(coastlines,add=T)                      # GZ "add=T" per aggiungere confini al vecchio plot senza eliminarlo -> grafico completo
+
+# GZ esercizio: plot della mappa di densità con nuova colorazione e aggiunta coastlines
+clr<-colorRampPalette(c('light blue','blue','pink','purple')) (400)
+plot(d,col=clr)
+plot(coastlines,add=T)
+setwd("C:/lab")
+load("C:/lab/point_ppattern.RData")
+library(spatstat)
+ls()
+library(rgdal)
+
+# GZ esercizio: plot mappa di densità con nuova palette
+coastlines<-readOGR("ne_10m_coastline.shp")
+clr<-colorRampPalette(c('light blue','blue','pink','purple')) (400)
+plot(d,col=clr,main="density")
+plot(coastlines,add=T)
+points(covids)
+
+# GZ Interpolation
+covid
+marks(covids)<-covid$cases  # "marks" -> associare dati categoria "cases" al pointpattern "covids"
+s<-Smooth(covids)           # "Smooth" -> creare mappa con i dati appena costruiti
+plot(s)                     # plot mappa appena creata
+
+# GZ esercizio: plot(s) con coastlines e punti
+cls<-colorRampPalette(c('light blue','blue','green'))(100)
+plot(s,col=cls,main="Cases")
+points(covids)
+plot(coastlines,add=T)
+
+# GZ mappa finale (multiframe con entrambi i plot fatti)
+par(mfrow=c(2,1))
+
+# GZ primo plot per densità
+clr<-colorRampPalette(c('light blue','blue','pink','purple')) (400)
+plot(d,col=clr,main="density")
+plot(coastlines,add=T)
+points(covids)
+
+# GZ secondo plot per interpolazione numero di casi
+cls<-colorRampPalette(c('light blue','blue','green'))(100)
+plot(s,col=cls,main="Cases")
+points(covids)
+plot(coastlines,add=T)
+
+# San Marino (lavorare con set di dati di una tesi su San Marino scaricati in "lab" )
+setwd("C:/lab")
+load("C:/lab/Tesi.RData")
+ls()
+head(Tesi)
+library(spatstat)
+attach(Tesi)
+
+# GZ Point pattern: x,y,c(xmin,xmax),c(ymin,ymax)
+summary(Tesi)  # sommario del dataset, posso trovare rapidamente le info principali
+# GZ "summary" -> longitudine: 12.42<x<12.46 e latitudine: 43.91<y<43.94
+Tesippp<-ppp(Longitude,Latitude,c(12.41,12.47),c(43.90,43.95))
+
+# GZ Mappa densità
+dT<-density(Tesippp)
+dev.off 
+plot(dT)
+points(Tesippp,col="green")
+
+# GZ set wd e richiamo libraries
+setwd("C:/lab")
+load("C:/lab/Tesi.RData")
+library(spatstat)
+library(rgdal)
+
+# GZ dt=density map, Tesi=dataset, Tesippp=point pattern (coordinate longitudine e latitudine)
+
+head(Tesi)
+
+# GZ associare al point pattern il valore d'interesse (ricchezza di specie) e poi procedere con l'interpolazione
+marks(Tesippp)<-Tesi$Species_richness
+interpol<-Smooth(Tesippp)
+plot(interpol)                         # GZ mappa
+points(Tesippp)
+
+# GZ caricare il file vettoriale "San_Marino" e sovrapponiamo la mappa costruita prima (così da avere i confini)
+sanmarino<-readOGR("San_Marino.shp")
+plot(sanmarino)
+plot(interpol,add=T)                  # GZ "add=T" per indicare che mappa di interpolazione sovrapposta a mappa di San Marino
+points(Tesippp)
+plot(sanmarino,add=T)                 # GZ -> vedere nuovamente confini
+
+# GZ esercizio: plot multiframe densità e interpolazione (due righe, una colonna)
+par(mfrow=c(2,1))
+plot(dT,main="Density of points")
+points(Tesippp)
+plot(interpol,main="Estimate of species richness")
+points(Tesippp)
+
+# GZ esercizio: come prima ma due colonne e una riga
+par(mfrow=c(1,2))
+plot(dT,main="Density of points")
+points(Tesippp)
+plot(interpol,main="Estimate of species richness")
+points(Tesippp)
+
+#######################################################
+#######################################################
+
+
 
 
 
