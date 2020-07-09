0d32f072ee78b4789f3220ee8fdf1b56e7ac984b
GiovanniZanfei <62332774+GiovanniZanfei@users.noreply.github.com>
2020-07-09 11:05:40 +0200

    Update and rename R_code_multitemp_NO2.r to 06_R_code_multitemp_NO2.r

--- 06_R_code_multitemp_NO2.r
+++ 06_R_code_multitemp_NO2.r
@@ -1,12 +1,13 @@
 # R code for analisyng NO2 data from ESA - January to March 2020
+
 setwd("C:/lab")
 library(raster)
 
-# Importare immagini
+# importare immagini -> "raster" perchè immagine con una sola banda (con più bande si usa "brick")
 EN01<-raster("EN_0001.png")
-plot(EN01)
+plot(EN01) 
 
-# Eserizio: importare tutte le immagini
+# eserizio: importare tutte le immagini
 EN02<-raster("EN_0002.png")
 EN03<-raster("EN_0003.png")
 EN04<-raster("EN_0004.png")
@@ -20,7 +21,7 @@ EN11<-raster("EN_0011.png")
 EN12<-raster("EN_0012.png")
 EN13<-raster("EN_0013.png")
 
-# plot multiframe immagine inizale-finale
+# plot multiframe immagine inizale-finale => confronto
 cl<-colorRampPalette(c('red','orange',yellow'))(100)
 par(mfrow=c(1,2)
 plot(EN01,col=cl)
@@ -28,11 +29,12 @@ plot(EN13,col=cl)
 
 dev.off()
 
-# Differenza tra EN13 e EN01
+# differenza "EN13" - "EN01"
 difno2<-EN13-EN01
 cldif<-colorRampPalette(c('blue','black','yellow'))(100)
+plot(difno2,col=cldif)
 
-# Esercizio: plot di tutte le mappe
+# esercizio: plot multiframe tutte le immagini
 par(mfrow=c(4,4))
 plot(EN01,col=cl)
 plot(EN02,col=cl)
@@ -49,35 +51,33 @@ plot(EN12,col=cl)
 plot(EN13,col=cl)
 # in alternativa si può fare: plot(EN01,EN02,EN03,EN04,EN05,EN06,EN07,EN08,EN09,EN10,EN11,EN12,EN13,col=cl)
 
-# 6/05
+# Day2
 
 setwd("C:/lab")
 load("EN.RData")
 ls()
 library(raster)
-
+# "list.files" (pacchetto "raster") -> vettore comprendente lista di file in una data directory (cartella creata appositamente - "esa_no2")
 setwd("C:/lab/esa_no2")
 rlist<-list.files(pattern=".png")
 rlist
 
-# la funzione "lapply" applica la funzione raster ad una lista di dati anzichè ad un solo file
-# lapply per caricare un lista di dati tutti insieme
+# "lapply" -> applica funzione indicata ad una lista (anzichè ad un solo file)
+# in questo caso "raster" -> importare lista di immagini
 listafinale<-lapply(rlist,raster)
 listafinale
 
-# stack per trasformare la lista in una sorta di immagine di n bande (13 in questi caso), come se fosse un set multitemporale. Poi plottata
+# "stack" -> trasformare lista in una sorta di immagine di n bande (13 in questi caso), come fosse un set multitemporale
 EN<-stack(listafinale)
 cl<-colorRampPalette(c('red','orange',yellow'))(100)
-plot(EN,col=cl)  # con questo plot visualizzo le immagini contenute nello stack EN
+plot(EN,col=cl)                                       # -> visualizzare immagini contenute nello stack "EN"
 
-# boxplot EN
+# differenza marzo ("EN13) - gennaio ("EN01")
+difEN<-EN$EN_0013-EN$EN_0001
+cld<-colorRampPalette(c('blue','white','red'))(100)
+plot(difEN,col=cld)
+
+# boxplot EN -> confronto tra tutte le immagini creando diagramma a riquadri (indicando caratteristiche grafiche)
 boxplot(EN,horizontal=T, # barre boxplot orizzontali
         outline=F,       # elimina outliners
         axes=T)          # presenta assi nel plot
-
-
-
-
-
-
-
