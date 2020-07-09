b3621e2bc20633007748ec3af704b23668cd739b
GiovanniZanfei <62332774+GiovanniZanfei@users.noreply.github.com>
2020-07-09 11:18:07 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -7,10 +7,10 @@
 # 2. 02_R_code_spatial.r	
 # 3. 03_R_code_point_patterns.r
 # 4. 04_R_code_TeleRil.r		
-# 6. R_code_multitemp.r	
-# 7. R_code_multitemp_NO2.r	
-# 8. R_code_snow.r	            #da far
-# 9. R_code_patches.r          # da far
+# 5. 05_R_code_multitemp.r	
+# 6. 06_R_code_multitemp_NO2.r	
+# 7. R_code_snow.r	            #da far
+# 8. R_code_patches.r          # da far
 
 #############################################################################
 #############################################################################
@@ -271,6 +271,7 @@ plot(coastlines,add=T)
 points(covids)
 
 ### GZ INTERPOLATION
+
 covid
 marks(covids)<-covid$cases  # "marks" -> associare dati categoria "cases" al pointpattern "covids"
 s<-Smooth(covids)           # "Smooth" -> creare mappa con i dati appena costruiti
@@ -599,3 +600,143 @@ par(mfrow=c(2,1))
 plot(difdvi,col=cldifdvi)
 plot(difdvilr50,col=cldifdvi)
 
+#############################################################################
+#############################################################################
+
+5. 05_R_code_multitemp.r - Analisi multitemporale variazione landcover
+
+# GZ setwd e pacchetti
+setwd("C:/lab")
+library(raster)
+library(RStoolbox)
+library(ggplot2)
+
+# GZ importare immagini
+defor1 <-brick("defor1_.png")
+defor2 <-brick("defor2_.png")
+
+# GZ plotRGB "defor1"
+defor1                                      # GZ visualizzare i campi dell'oggetto
+# "defor1_.1" "defor1_.2" "defor1_.3"
+# defor1_.1 = NIR
+# defor1_.2 = red
+# defor1_.3 = green
+plotRGB(defor1,r=1,g=2,b=3,stretch="Lin")   # GZ banda red->nir(r=1), green->red(g=2), blue->green(b=3)
+
+# GZ eserczio: plot seconda data
+plotRGB(defor2,r=1,g=2,b=3,stretch="Lin")
+
+# GZ confronto (multiframe) stessa area in momenti differenti (prima e dopo deforestazione)
+par(mfrow = c(2,1))
+plotRGB(defor1,r=1,g=2,b=3,stretch="Lin")
+plotRGB(defor2,r=1,g=2,b=3,stretch="Lin")
+
+dev.off()
+
+# GZ classificazione non supervisionata
+d1c<-unsuperClass(defor1,nClasses=2)             # GZ si creano classi di foresta e non foresta (suddivisione pixel in queste due categorie)
+plot(d1c$map)
+cl<-colorRampPalette(c('green','black'))(100)
+plot(d1c$map,col=cl)
+
+# GZ esercizio: come prima per "defor2"
+d2c<-unsuperClass(defor2,nClasses=2)              
+plot(d2c$map,col=cl)
+
+dev.off()
+
+# GZ confronto tra i due momenti (multiframe) con pixel classificati
+# GZ due righe, una colonna
+par(mfrow=c(2,1))
+plot(d1c$map,col=cl)
+plot(d2c$map,col=cl)
+# GZ due colonne, una riga
+par(mfrow=c(1,2))
+plot(d1c$map,col=cl)
+plot(d2c$map,col=cl)
+
+dev.off()
+
+# GZ calcolo frequenza delle due classi di pixel nella prima immagine
+freq(d1c$map)
+# GZ aree aperte=37039
+# GZ foresta=304253
+
+# GZ numero di pixel totali nella prima immagine (necessario per calcolo percentuale)
+totd1<-37039+304253       
+totd1
+# totd1=341292
+
+# GZ calcolo frequenze percentuali
+percent1<-freq(d1c$map)*100/totd1
+# GZ foreste: 89.1 %
+# GZ aree aperte: 10.9 %
+
+# GZ stesso procedimento per la seconda immagine
+freq(d2c$map)
+# GZ aree aperte=165055
+# GZ foreste=177671
+
+totd2<-165055+177671
+totd2
+# GZ totd2=342726
+
+percent2<-freq(d2c$map)*100/totd2
+# GZ aree aperte: 48.2 %
+# GZ foreste: 51.8 %
+
+# GZ creare vettori per analisi grafica
+cover<-c("Agriculture","Forest")
+before<-c(10.9,89.1)
+after<-c(48.2,51.8)
+
+# GZ creare nuovo dataset con i dati ottenuti
+output<-data.frame(cover,before,after)
+output
+
+# Day2
+
+setwd("C:/lab")
+load("C:/lab/defor.RData")
+library(raster)
+library(ggplot2)
+install.packages("gridExtra")
+library(gridExtra)
+
+# GZ riprendere mappe 
+par(mfrow=c(1,2))
+cl<-colorRampPalette(c('black','green'))(100)
+plot(d1c$map,col=cl)
+plot(d2c$map,col=cl)
+ls()
+output
+#        cover before after
+# 1 Agriculture   10.9  48.2
+# 2      Forest   89.1  51.8
+
+# GZ istogramma delle percentuali di copertura prima della deforestazione
+grafico1<-ggplot(output,aes(x=cover,y=before,color=cover)) +
+geom_bar(stat="identity",fill="white")
+grafico1                                                        # GZ in ascissa "aes"(aree di foresta/aperte), in ordinata percentuale di copertura
+
+# GZ esercizio: istogramma dopo deforestazione
+grafico2<-ggplot(output,aes(x=cover,y=after,color=cover)) +
+geom_bar(stat="identity",fill="white")
+grafico2
+
+# GZ esercizio: usare "grid.arrange" (funzione del pacchetto "gridExtra" che permette confronto tra istogrammi) per creare un plot con grafico1 e grafico2
+# GZ grid.arrange=> grid.arrange(plot1,plot2,nrow=1), questa funzione crea un plot con più grafici
+grid.arrange(grafico1,grafico2,nrow=1)
+# GZ evidente cambiamento nelle percentuali di copertura
+
+#GZ  per facilitare confronto uniformare sacla dei due istogrammi (imporre al grafico il limite y=100)
+grafico1<-ggplot(output,aes(x=cover,y=before,color=cover)) +
+geom_bar(stat="identity",fill="white") + 
+ylim(0,100)
+grafico2 <- ggplot(output, aes(x=cover, y=after, color=cover)) + 
+geom_bar(stat="identity", fill="white") +
+ylim(0, 100)
+grid.arrange(grafico1,grafico2,nrow=1)
+
+#############################################################################
+#############################################################################
