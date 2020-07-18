52d01f78a2ae1501c782b77de2359c4814b42398
GiovanniZanfei <62332774+GiovanniZanfei@users.noreply.github.com>
2020-07-18 22:20:01 +0200

    Update 11_R_code_examproject.r

--- 11_R_code_examproject.r
+++ 11_R_code_examproject.r
@@ -7,41 +7,79 @@ library(raster)
 
 # importare dati albedo e raggrupparli
 examlist<-list.files(pattern=".nc",full.names=T)
-list_rast<-lapply(examlistlist,raster)
+list_rast<-lapply(examlist,raster)
 alb.multitemp<-stack(list_rast)
+
+# rinominare i file per praticità
+alb.jan2000<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.1
+alb.jan2010<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.2
+alb.jan2020<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.3
+alb.jul2000<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.4
+alb.jul2010<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.5
+alb.jul2020<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.6
 
 # plot
 cl<-colorRampPalette(c('red','orange','yellow'))(100) 
-plot(alb,multitemp,col=cl,zlim=c(0,0.7))
+plot(alb.multitemp,col=cl,zlim=c(0,0.7))
 
-# rinominare i file per praticità
-albjan2000<-
-albjan2010<-
-albjan2020<-
-albjul2000<-
-albjul2010<-
-albjul2020<-
+# boxplot -> vedere andamento
+boxplot(alb.multitemp,horizontal=T,outline=F)
 
 # confronto jan
 par(mfrow=c(1,3))
-plot(albjan2000)
-plot(albjan2010)
-plot(albjan2020)
+plot(alb.jan2000,col=cl,zlim=c(0,0.7))
+plot(alb.jan2010,col=cl,zlim=c(0,0.7))
+plot(alb.jan2020,col=cl,zlim=c(0,0.7))
 
 # confronto jul
 par(mfrow=c(1,3))
-plot2000
-plot2010
-plot2020
+plot(alb.jul2000,col=cl,zlim=c(0,0.7))
+plot(alb.jul2010,col=cl,zlim=c(0,0.7))
+plot(alb.jul2020,col=cl,zlim=c(0,0.7))
 
-# quantificare differenze notare dove c'è differenza più evidente e fare calcolo della differenza
-diff
-cldif
-plot
+# quantificare differenze albedo => se varia di più in inverno o estate, in che decennio il cambiamento è massimo
+dif1<-alb.jan2000-alb.jan2010
+dif2<-alb.jan2010-alb.jan2020
+dif3<-alb.jul2000-alb.jul2010
+dif4<-alb.jul2010-alb.jul2020
+cldif<-colorRampPalette(c('blue','white','red'))
+plot(dif1,col=cldif)
+plot(dif2,col=cldif)
+plot(dif3,col=cldif)
+plot(dif4,col=cldif)
 
-# crop arco alpino
+# differenza 2000-2020 nella stagione con cambio più evidente => confronto con dati snow usati a lezione
+difmax(alb.x2000-alb.x2020)
+plot(difmax,col=cldif)
 
-# confronto crop arco alpino copertura nevosa
+# importare raster copertura nevosa
+snowlist<-list.files(pattern="snow",full.names=T)
+list_snow<-lapply(snowlist,raster)
+snow.multitemp<-stack(list_snow)
+
+# confronto con copertura nevosa
+clsnow<-colorRampPalette(c('darkblue','blue','light blue'))(100) 
+par(mfrow=c(2,2))
+plot(snow.multitemp$snow2000r,col=clsnow,zlim=c(0,250))
+plot(snow.multitemp$snow2020r,col=clsnow,zlim=c(0,250))
+plot(alb.x2000,col=cl,zlim=c(0,0.7))
+plot(alb.x2020,col=cl,zlim=c(0,0.7))
+
+# confronto differenze albedo-snow
+difsnow<-snow.multitemp$snow2020r-snow.multitemp$snow2000r
+par(mfrow=c(1,2))
+plot(difmax,col=cldif)
+plot(difsnow,col=cldif)
+
+# crop arco alpino (albedo e snow)
+extension<-c(6,20,a,b)
+dif.alb.alps<-crop(difmax,extension)
+dif.snow.alps<-crop(difsnow,extension)
+par(mfrow=c(1,2))
+plot(dif.alb.alps,col=cldif)
+plot(dif.snow.alps,col=cldif)
+
+
 
 
 
