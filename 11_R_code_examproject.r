44011b4721bfdab2c875d279159a0538b2ff7543
GiovanniZanfei <62332774+GiovanniZanfei@users.noreply.github.com>
2020-07-18 17:28:45 +0200

    Update 11_R_code_examproject.r

--- 11_R_code_examproject.r
+++ 11_R_code_examproject.r
@@ -14,16 +14,35 @@ alb.multitemp<-stack(list_rast)
 cl<-colorRampPalette(c('red','orange','yellow'))(100) 
 plot(alb,multitemp,col=cl,zlim=c(0,0.7))
 
-# confronto
-2000-2010-2020
-2000-2010-2020
+# rinominare i file per praticità
+albjan2000<-
+albjan2010<-
+albjan2020<-
+albjul2000<-
+albjul2010<-
+albjul2020<-
 
-# quantificare differenze?
+# confronto jan
+par(mfrow=c(1,3))
+plot(albjan2000)
+plot(albjan2010)
+plot(albjan2020)
 
-# importare, plot e confronto anche per copertura nevosa
+# confronto jul
+par(mfrow=c(1,3))
+plot2000
+plot2010
+plot2020
 
-# confronto albedo copertura nevosa
+# quantificare differenze notare dove c'è differenza più evidente e fare calcolo della differenza
+diff
+cldif
+plot
 
-# crop alpi
+# crop arco alpino
+
+# confronto crop arco alpino copertura nevosa
+
+
 
 
