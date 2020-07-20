cb040f43f149d1d1087a29dbdab1f2c3c6df8b7a
GiovanniZanfei <62332774+GiovanniZanfei@users.noreply.github.com>
2020-07-20 16:51:27 +0200

    Update 11_R_code_examproject.r

--- 11_R_code_examproject.r
+++ 11_R_code_examproject.r
@@ -12,10 +12,10 @@ alb.multitemp<-stack(list_rast)
 
 # rinominare i file per praticità
 alb.jan2000<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.1
-alb.jan2010<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.2
-alb.jan2020<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.3
-alb.jul2000<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.4
-alb.jul2010<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.5
+alb.jan2010<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.3
+alb.jan2020<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.5
+alb.jul2000<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.2
+alb.jul2010<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.4
 alb.jul2020<-alb.multitemp$Broadband.directional.albedo.over.total.spectrum.6
 
 # plot
@@ -40,12 +40,10 @@ plot(alb.jul2020,col=cl,zlim=c(0,0.9))
 # quantificare differenze albedo => variazione più evidente in estate o inverno
 dif1<-alb.jan2000-alb.jan2020
 dif2<-alb.jul2000-alb.jul2020
-cldif<-colorRampPalette(c('blue','white','red'))(100)
+cldif<-colorRampPalette(c('blue','white','red'))(100)  # in rosso calo albedo, in blu aumento    
 par(mfrow=c(1,2))
 plot(dif1,col=cldif)
-plot(dif2,col=cldif)
-
-# potrebbe essere interessante crop su deserto del Sahara
+plot(dif2,col=cldif) # cal albedo più evidente nella stagione estiva
 
 # confronto con copertura nevosa => atteso pattern simile
 # importare raster copertura nevosa
